a6318910d27125a2825a65cfbe78438bb09858f6
Natee <gg977120@gmail.com>
2022-01-25 23:34:33 +0700

    test 2

--- AC Circuit Analyzer.cpp
+++ AC Circuit Analyzer.cpp
@@ -4,11 +4,22 @@
 #include<complex>
 using namespace std;
 
+double voltage;
+double angular_frequency;
+double offset;
+
+void input();
+void output();
 
 int main(){
-    cout << "Sawasdee";
-    cout<< "dee";
-    cout<<"edok";
-    cout << "this is joke";
+    cout << "Please Enter AC voltage function (Sinusoids)";
+    cout << "Voltage :";
+    cin >> voltage;
+    cout << "angular_frequency :";
+    cin >> angular_frequency;
+    cout << "offset :";
+    cin >> offset;
+    cout << "Your's function:" << voltage << "con(" << angular_frequency << "t" << offset << ")";
+
     return 0;
 }
