8ca8024ee7d4c69bac3eba5d38369075026962de
Natee <gg977120@gmail.com>
2022-01-27 22:23:58 +0700

    Update 27/01 22:23

--- AC_Circuit_Analyzer.cpp
+++ AC_Circuit_Analyzer.cpp
@@ -8,40 +8,44 @@ using namespace std;
 
 double voltage;
 double angular_frequency;
-double offset;
-int high = 10, wide = 20;
+double offset; 
+int high = 10, wide = 20; // ค่าเริ่มต้นของขนาดตาราง
 
-void input(string **,vector<string>);
-void drawing(string **);
-void drawing_prossing(string **);
-void rectangulartopolar(double,double,double); // convert rectangular function to polar from
-void output();
+void drawing(string **canvas, vector<string>  componant); // รับข้อมูลเป็น ชนิดอุปกรณ์ ตำแหน่ง(หัวและท้าย(x1 y1 x2 y2))(ชนิดอุปกรณ์:ตำแหน่งy1:ตำแหน่งx1:ตำแหน่งy2:ตำแหน่งx2:ค่าของตัวแปรเช่น 10H(เฮนรี่)) ตัวแปลอุปกรณ์ และวาดออกมาเป็นวงจร
+void input(string **canvas,vector<string> componant); // รับข้อมูล ชนิดอุปกรณ์ ตำแหน่ง(หัวและท้าย(x1 y1 x2 y2)) ตัวแปลอุปกรณ์ ลงใน vector<string> componant
+void polar_to_rectangular(vector<string> componant, vector<string> componant_rectangular); // แปลงค่าของอุปกรณ์ในรูป polar ให้อยู่ในรูป rectangular เช่น 10F เป็น 1/jwF
+void analysis(vector<string> componant, vector<string> componant_rectangular); // วิเคราะห์วงจรโดยอิงค่า Z จาก componant_rectangular และตำแหน่งของอุปกรณ์จาก componant แล้วคืนค่าเป็นค่า Z รวม
+//void complex_processing("ส่งในรูปของ <complex>"); // รับค่า complex จากฟังก์ชัน analysis คำนวน บวก ลบ คูณ หาร และคืนกลับเป็นค่า complex
+//void rectangular_to_polar("ส่งในรูปของ <complex>"); // รับค่า complex ในตอนจบของจากฟังก์ชัน analysis แปลงค่าของอุปกรณ์ในรูป rectangular ให้อยู่ในรูป polar (ส่วนกลับของฟังก์ชัน polar_to_rectangular) ดูจากที่ปักหมุดใน Discord
+void output(string Z, double angular_frequency_process); // รับค่าความถี่เชิงมุมจากฟังก์ชัน rectangular_to_polar และตัวแปล voltage offset จาก global แล้วนำมาแสดงผลในรูปฟังก์ชัน I = "voltage"cos("angular_frequency"+-"offset") พร้อมทั้งแสดงผลค่าความต้านทานรวมและฟังก์ชันในรูป sinusoid
 
 int main(){
     vector<string> componant;
+    vector<string> componant_rectangular;
+    string Z;
     int voltage_source_position = 1;
-    /*cout << "Please Enter AC voltage function (Sinusoids)" << endl;
+    cout << "Please Enter AC voltage function (Sinusoids)" << endl;
     cout << "Voltage :";
     cin >> voltage;
     cout << "angular_frequency :";
     cin >> angular_frequency;
     cout << "offset :";
     cin >> offset;
-    cout << "Your's function:" << voltage << "cos(" << angular_frequency << "t" << offset << ")";
+    cout << "Your's function:" << voltage << "cos(" << angular_frequency << "t" << offset << ")" << endl;
     cout << "input your built plate(high wide):";
     cin >> high;
-    cin >> wide;*/
+    cin >> wide;
     
     string **canvas = new string*[high];
     for(int i = 0; i < high; i++) canvas[i] = new string[wide];
-    drawing(canvas);
+    drawing(canvas, componant);
     cout << "Place voltage source at:";
     cin >> voltage_source_position;
     canvas[(voltage_source_position-1)/10][(voltage_source_position-1)%10] = "@";
     cout << "voltage scource reference connent to:";
     cin >> voltage_source_position;
     canvas[(voltage_source_position-1)/10][(voltage_source_position-1)%10] = "R";
-    drawing(canvas);
+    drawing(canvas, componant);
     input(canvas, componant);
     
     for(int i = 0; i < high; i++) delete [] canvas[i];
@@ -49,10 +53,10 @@ int main(){
     return 0;
 }
 
-void drawing(string **canvas){
+void drawing(string **canvas, vector<string>  componant){
     int count = 1;
     for(int i = 0; i < high; i++){
-        for(int j = 0;j < wide; j++){
+        for(int j = 0; j < wide; j++){
             if(canvas[i][j] == "") cout << setw(4) << count;
             else cout << setw(4) << canvas[i][j];
             count++;
@@ -74,10 +78,13 @@ void input(string **canvas, vector<string> componant){
         if(componant_type != "5"){
             cout << "pick first connect point and the point that it connect to (position x,y and position x,y) :";
             cin >> position_x1 >> position_y1 >> position_x2 >> position_y2;
+            if(atof(position_x1.c_str())*atof(position_y1.c_str()) > high*wide || atof(position_x2.c_str())*atof(position_y2.c_str()) > high*wide){
+                cout << "Error off canvas limit" << endl;
+                continue;
+            }
             cout << "input your componant_value(value follow by unit) :";
             cin >> componant_value;
-            combin+=(componant_type+":"+position_y1+":"+position_x1+":"+position_y2+":"+position_x2+":"+componant_value);
-            componant.push_back(combin);
+            componant.push_back(componant_type+":"+position_x1+":"+position_y1+":"+position_x2+":"+position_y2+":"+componant_value);
         }
         else if(componant_type == "5"){
             unsigned int pick;
@@ -90,8 +97,6 @@ void input(string **canvas, vector<string> componant){
         cout << endl;
     }
     
-    
-    
-    //"capacitor : position y : position x : componant value"
+    //ชนิดอุปกรณ์:ตำแหน่งx1:ตำแหน่งy1:ตำแหน่งx2:ตำแหน่งy2:ค่าของตัวแปรเช่น 10H(เฮนรี่)
 }
 //void drawing_prossing(string **canvas)
