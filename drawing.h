1077178fbf18ba41ab11492ade5894f8678b65ec
suwichada <jippee.1211@gmail.com>
2022-02-20 14:47:30 +0700

    20/2/2565

--- drawing.h
+++ drawing.h
@@ -8,57 +8,57 @@
 #include<vector>
 #include<complex>
 #include<sstream>
+#include<list>
 
 #include"struct.h"
 
 using namespace std;
 
 void drawing_add(string **canvas,vector<componant_struct> &componants, int high, int wide){
-    int count = 1,sum=1,count1=0;
+    int count = 1,count1=0;
     for(int i = 0; i < high; i++){
-        for(int j = 0; j < wide; j++){
-            sum=(i*10)+j;    
+        for(int j = 0; j < wide; j++){    
             count1 = componants[i].componant_in_connect;
             if(componants[i].componant_type == 1){
-                if(componants[i].componant_in_connect == sum) canvas[i][j-1]="o--";
+                if(componants[i].componant_in_connect == (i*10)+j) canvas[i][j-1]="o--";
                 if(componants[i].componant_out_connect-count1 !=1){
                         while (componants[i].componant_out_connect-count1 > 1){
                             canvas[(count1)/10][(count1)%10]="--";
                             count1++;
                         }
                 }
-                if(componants[i].componant_out_connect == sum)  canvas[i][j-1]="--o"; 
+                if(componants[i].componant_out_connect == (i*10)+j)  canvas[i][j-1]="--o"; 
             }
             if(componants[i].componant_type == 2){
-                if(componants[i].componant_in_connect== sum) canvas[i][j-1]="o-M";
+                if(componants[i].componant_in_connect== (i*10)+j) canvas[i][j-1]="o-M";
                 if(componants[i].componant_out_connect-count1 !=1){
                         while (componants[i].componant_out_connect-count1 > 1){
                             canvas[(count1)/10][(count1)%10]="MW";
                             count1++;
                         }
                 }
-                if(componants[i].componant_out_connect== sum) canvas[i][j-1]= "W-o";
+                if(componants[i].componant_out_connect== (i*10)+j) canvas[i][j-1]= "W-o";
             }
             if(componants[i].componant_type == 3){
-                if(componants[i].componant_in_connect== sum)canvas[i][j-1]="o-|";
+                if(componants[i].componant_in_connect== (i*10)+j)canvas[i][j-1]="o-|";
                 if(componants[i].componant_out_connect-count1 !=1){
                         while (componants[i].componant_out_connect-count1 > 1){
                             canvas[(count1)/10][(count1)%10]="||";
                             count1++;
                         }
                 }
-                if(componants[i].componant_out_connect== sum)canvas[i][j-1]="|-o";
+                if(componants[i].componant_out_connect== (i*10)+j)canvas[i][j-1]="|-o";
                     
             }
             if(componants[i].componant_type == 4){
-                if(componants[i].componant_in_connect== sum) canvas[i][j-1]= "o-o";
+                if(componants[i].componant_in_connect== (i*10)+j) canvas[i][j-1]= "o-o";
                 if(componants[i].componant_out_connect-count1 !=1){
                         while (componants[i].componant_out_connect-count1 > 1){
                             canvas[(count1)/10][(count1)%10]="oo";
                             count1++;
                         }
                 }
-                if(componants[i].componant_out_connect== sum)canvas[i][j-1]= "o-o";
+                if(componants[i].componant_out_connect== (i*10)+j)canvas[i][j-1]= "o-o";
             }
         }
     }
@@ -83,5 +83,19 @@ void drawing(string **canvas, vector<componant_struct> &componants, int high, in
         cout << "\n\n";
     }
 }
+/*bool chack_drawing(string **canvas, vector<componant_struct> &componants, int high, int wide){
+    int count = 1;
+    list<int> componants_connect = {0,0};
+    for(int i = 0; i < high; i++){
+        for(int j = 0; j < wide; j++){
+          if(componants_connect.empty){
+            componants_connect.pop_back(componants[i].componant_in_connect);
+            break;
+            }
+
+        }
+    }
+}*/
+
 
 #endif
